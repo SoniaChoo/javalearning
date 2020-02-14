3fbfdf5abc197ca5bba5588225cdd324eed0b264
Sonia <soniachoo.zhu@gmail.com>
2020-02-14 20:12:13 +0800

    hello world

---
+++ src/cainiao/zz.java
@@ -0,0 +1,7 @@
+package cainiao;
+
+public class zz {
+    public static void main(String []args) {
+        System.out.println("I am sonia");
+    }
+}
---
+++ src/day0214/forfordemo.java
@@ -0,0 +1,10 @@
+public class forfordemo {
+    public static void main(String  []args) {
+        for (int x = 1; x <= 9; x++) {
+            for (int y = 1; y <= x; y++) {
+                System.out.print(y + "*" + x + "=" +  x*y + "\t");
+            }
+            System.out.println();
+        }
+    }
+}
